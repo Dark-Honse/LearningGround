7e174679476f3df9e6feccd256a8e62a1b6989d9
Jamie Clynes <jamietclynes@hotmail.co.uk>
2025-05-18 10:06:05 +0100

    Exploring list functions

--- Lists.py
+++ Lists.py
@@ -27,6 +27,72 @@ print(main_characters)
 
 print(main_characters[1])
 
+print("--------------------------")
+print("Other list functions")
+
+lucky_numbers = [6, 51, 19, 25, 2, 91]
+
+print(lucky_numbers)
+
+main_characters.extend(lucky_numbers)
+
+print(main_characters)
+
+main_characters.append("Altair")
+
+print(main_characters)
+
+main_characters.insert(1, "Clatt")
+
+print(main_characters)
+
+main_characters.remove("Clatt")
+
+print(main_characters)
+
+main_characters.clear()
+
+print(main_characters)
+
+main_characters = ["Dan", "Zenith", "Brutus", "Clara", "Sid"]
+
+print(main_characters)
+
+main_characters.pop()
+
+print(main_characters)
+
+main_characters.append("Sid")
+
+print(main_characters.index("Zenith"))
+
+print(main_characters.count("Brutus"))
+
+main_characters.sort()
+
+print(main_characters)
+
+lucky_numbers.sort()
+
+print(lucky_numbers)
+
+lucky_numbers.reverse()
+
+print(lucky_numbers)
+
+main_characters2 = ["Dan", "Zenith", "Brutus", "Clara", "Sid"]
+
+main_characters2.insert(1, "Zenith (Ascended)")
+main_characters2.remove("Zenith")
+
+print(main_characters2)
+
+print(main_characters)
+
+main_characters2.sort()
+
+print(main_characters2)
+
 
 
 
