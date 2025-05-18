0812f18ca7839f52365c918cd76e6bd4846cf127
Jamie Clynes <jamietclynes@hotmail.co.uk>
2025-05-18 10:45:45 +0100

    Super basic calculator for addition

--- Basic Calculator.py
+++ Basic Calculator.py
@@ -1,1 +1,15 @@
 print("Building a basic calculator")
+
+number1 = input("Enter a number: ")
+number2 = input("Enter another number: ")
+
+print(number1 + number2)
+print("incorrect way to add two numbers. ")
+print("------------------------------")
+
+number1 = input("Enter a number: ")
+number2 = input("Enter another number: ")
+
+print(float(number1) + float(number2))
+
+
