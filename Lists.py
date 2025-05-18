f673498c4afe2af18c4d1d43962ac4606e7df93f
Jamie Clynes <jamietclynes@hotmail.co.uk>
2025-05-18 09:08:12 +0100

    Added subtitles to indicate training subject sections

--- Lists.py
+++ Lists.py
@@ -1,3 +1,5 @@
+print("Lists and accessing specific indexes")
+
 main_characters = ["Dan", "Zenith", "Brutus", "Clara", "Sid"]
 
 print(main_characters)
@@ -15,11 +17,16 @@ print(main_characters[-1:])
 print(main_characters[-3:])
 
 print("--------------------------")
+print("Modifying values within a list")
+
+print(main_characters)
 
 main_characters[1] = "Zenith (Ascended)"
 
 print(main_characters)
 
 print(main_characters[1])
+
+
 
 
