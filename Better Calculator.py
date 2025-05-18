dd87972e6a18ad28d191364c5d8d01cd11e614f8
Jamie Clynes <jamietclynes@hotmail.co.uk>
2025-05-18 17:33:06 +0100

    created a better calculator using if statements

--- Better Calculator.py
+++ Better Calculator.py
@@ -1,1 +1,20 @@
 print("A better calculator")
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator: ")
+num2 = float(input("Enter second number: "))
+ops = ["+", "-", "*", "/"]
+
+if op == "+":
+    result = num1 + num2
+elif op == "-":
+    result = num1 - num2
+elif op == "*":
+    result = num1 * num2
+elif op == "/":
+    result = num1 / num2
+else:
+    print("Invalid operator")
+
+if op == "+" or op == "-" or op == "*" or op == "/":
+    print(result)
