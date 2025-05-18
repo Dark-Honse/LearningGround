622a0885d7344ad2fd0de34809e2e4787b9afd56
Jamie Clynes <jamietclynes@hotmail.co.uk>
2025-05-18 16:33:41 +0100

    some if statement practice

---
+++ More on if statements.py
@@ -0,0 +1,13 @@
+is_red = False
+is_blue = False
+
+if is_red and is_blue:
+    print("The car is red and blue")
+elif is_red and not is_blue:
+    print("The car is red")
+elif not is_red and is_blue:
+    print("The car is blue")
+else:
+    print("The car is neither red nor blue")
+    car_colour = input("What is the colour of the car? ")
+    print(f"The car is {car_colour}")
