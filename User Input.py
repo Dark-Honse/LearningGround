7456a0d6abae45fc2213fe6ea1e8f8584ed3c79e
Jamie Clynes <jamietclynes@hotmail.co.uk>
2025-05-18 10:34:27 +0100

    Basic user input with prompts and response

--- User Input.py
+++ User Input.py
@@ -1,3 +1,13 @@
 print("Exploring user input")
 
 name = input("Enter name: ")
+
+print("Hello, " +name)
+print("how old are you? ")
+
+age = input("Age: ")
+
+print("Nice! I wish I was " + age)
+
+
+
