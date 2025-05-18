ef537e2aeb4c2221ee759509c81406b2019586d2
Jamie Clynes <jamietclynes@hotmail.co.uk>
2025-05-18 14:46:23 +0100

    return function

--- Functions.py
+++ Functions.py
@@ -12,3 +12,18 @@ def sayhi1(name):
 
 sayhi1("Peter")
 sayhi1("Daniel")
+
+print("------------------")
+
+def cube(num):
+    return num*num*num
+
+print(cube(6))
+
+def cube2(num):
+    return num*num*num
+
+result = cube2(5)
+print(result)
+
+print("-------------------")
