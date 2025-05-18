c7e50904a49f19c5e39424623cd2e687ca41eeab
Jamie Clynes <jamietclynes@hotmail.co.uk>
2025-05-18 12:34:26 +0100

    introductory use of a basic function

--- Functions.py
+++ Functions.py
@@ -1,2 +1,14 @@
 print("Exploring functions")
 
+def sayhi():
+    print("Hello User")
+
+sayhi()
+
+print("-----------------")
+
+def sayhi1(name):
+    print("Hello " + name)
+
+sayhi1("Peter")
+sayhi1("Daniel")
