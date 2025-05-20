753a91c15622be0b3d87548b646676407c1e83ae
Dark-Honse <jamietclynes@hotmail.co.uk>
2025-05-20 20:13:18 +0100

    weapons dictionary from scratch

--- Dictionaries.py
+++ Dictionaries.py
@@ -10,3 +10,18 @@ if room_name in rooms:
     print(rooms[room_name])
 else:
     print("You made that up.")
+
+print("-----------------------")
+
+weapons = {
+    "dagger": "A small blade. Easy to conceal. Easy to wield.",
+    "revolver": "A rusted revolver with intricate line engravements. 5 bullet capacity.",
+    "nuclear warhead": "A massive, unwieldy atomic bomb. You can barely lift it up."
+}
+
+weapon = input("Choose a weapon: ").lower()
+
+if weapon in weapons:
+    print(weapons[weapon])
+else:
+    print("You made that up.")
