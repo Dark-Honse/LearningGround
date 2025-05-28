76b2f3d85abb79c1bbb0f3b8367f91b2c77f8299
Dark-Honse <jamietclynes@hotmail.co.uk>
2025-05-28 20:20:16 +0100

    added find oldest loop

--- CSV handling.py
+++ CSV handling.py
@@ -3,6 +3,8 @@ import csv
 age_count= 0
 total_age = 0
 ave_age = 0
+oldest = 0
+name = ""
 
 rating_count = 0
 total_rating = 0
@@ -45,9 +47,19 @@ with open("CSV Example.csv", encoding="utf-8-sig") as file:
         if int(row[2]) > 80:
             rating_count += 1
             total_rating += int(row[2])
-        ave_rating = total_rating / rating_count
-        print(ave_rating)
     ave_rating = total_rating / rating_count
     print(round(ave_rating))
+
+    ave_rating = 0
+    rating_count = 0
+    total_rating = 0
+
+    print("--------------")
+    for row in data:
+        if int(row[1]) >= oldest:
+            oldest = int(row[1])
+            name = str(row[0])
+    print(oldest)
+    print(f"{name} is the oldest at {oldest}")
 
 
